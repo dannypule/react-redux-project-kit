2facd5c632cdc921426113acde604542d8ea665f
dannypule <kojikusan@gmail.com>
2018-07-07 12:36:22 +0100

    add gridlex and toastify

--- app/components/Foo/Foo.js
+++ app/components/Foo/Foo.js
@@ -2,5 +2,10 @@ import React from 'react';
 import { Button } from 'semantic-ui-react';
 
 export default function Foo() {
-  return <Button primary>hello world</Button>;
+  return (
+    <div>
+      <Button primary>hello world</Button>
+      <i className="material-icons">face</i>
+    </div>
+  );
 }
