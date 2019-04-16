4dd2ade0ecf50e4dc489be412964f7d0b768e54b
dannypule <kojikusan@gmail.com>
2019-04-16 08:27:06 +0100

    remove unused code

--- app/redux/users/actions.js
+++ app/redux/users/actions.js
@@ -1,20 +1,5 @@
 import { createRoutine } from 'redux-saga-routines';
 
-// export const addUser = () => () => {
-//   apiService
-//     .post('/users', {
-//       firstName: 'duder',
-//       lastName: 'dudeson',
-//       email: 'duder.duder@fake-email.infozzz',
-//       password: 'admin',
-//       userRoleCode: 30,
-//       companyId: 2,
-//     })
-//     .then(data => {
-//       console.log(data);
-//     });
-// };
-
 export const GET_USERS = 'GET_USERS';
 export const ADD_USER = 'ADD_USER';
 
